101ac4ef01a081731f04c21be03509f2a743aa36
Oscar morales <27122323+Ev1lT3rm1nal@users.noreply.github.com>
2021-10-27 17:45:53 -0500

    Make value to constant

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::ffi::CString;
 use std::os::raw::c_char;
 
 // DON'T CHANGE THIS
@@ -8,24 +7,25 @@ pub enum ResultType {
     HIT,
     BAD,
 }
+
 // DON'T CHANGE THIS
 #[repr(C)]
 pub enum ProxyType {
     HTTPS,
     SOCKS4,
-    SOCKS5
+    SOCKS5,
 }
 
 //THE NAME OF YOUR PLUGIN
 #[no_mangle]
-pub extern "C" fn PLUGIN_NAME() -> *mut c_char {
-    CString::new("Name").unwrap().into_raw()
+pub extern "C" fn PLUGIN_NAME() -> *const c_char {
+    "name\0".as_ptr() as *const c_char
 }
 
 //THE NAME OF YOUR PLUGIN
 #[no_mangle]
 pub extern "C" fn PLUGIN_VERSION() -> *const c_char {
-    CString::new("0.0.1").unwrap().into_raw()
+    "0.0.1\0".as_ptr() as *const c_char
 }
 
 #[no_mangle]
@@ -33,7 +33,7 @@ pub extern "C" fn get_combo_result(
     user: *mut ::std::os::raw::c_char,
     pass: *mut ::std::os::raw::c_char,
     proxy: *mut ::std::os::raw::c_char,
-    proxy_type: ProxyType
+    proxy_type: ProxyType,
 ) -> ResultType {
     // ADD YOUR CODE HERE
 
